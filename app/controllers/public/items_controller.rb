273d229b488ae79d491522c1a7688175701b5fd5
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-01-13 02:28:23 +0000

    在庫一覧ページ作成中

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -5,13 +5,28 @@ class Public::ItemsController < ApplicationController
     @item = Item.new
   end
   
+  def edit
+    @item = Item.find(params[:id])
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to items_path
+  end
+  
   def index
+    @items = Item.all
   end
   
   def show
   end
   
-  def edit
-  end
+  # ストロングパラメータ
+  private
+    def item_params
+      params.require(:item).permit(:image, :name, :Purchase_amount, :Sale_amount, :Date_of_purchase, :genre)
+    end
   
 end
+
