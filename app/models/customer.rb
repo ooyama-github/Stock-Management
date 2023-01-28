768415d474ee863f7e663247542a5f6253a85e1a
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-01-28 04:22:44 +0000

    カスタマーモデル追記

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,16 @@ class Customer < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :genres, dependent: :destroy
 
+  has_one_attached :profile_image
+  # プロフィール画像を取得するためのメソッド
+  def get_profile_image(width,height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/noimage.png')
+      profile_image.attach(io: File.open(file_path),filename: 'default-image.png',content_type:'image/png')
+    end
+    profile_image.variant(gravity: :center, resize:"#{width}x#{height}^", crop:"#{width}x#{height}+0+0").processed
+  end
+
   # ゲストログイン用の記述
   def self.guest
    find_or_create_by!(name: 'ゲストユーザー' ,email: 'guestuser@example.com') do |customer|
